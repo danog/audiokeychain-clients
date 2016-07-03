989006963f8b2b2e48222f4d50b59f10623500aa
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-07-03 14:38:24 +0200

    Update

--- bot/commands.sh
+++ bot/commands.sh
@@ -12,12 +12,15 @@ if [ "$1" = "source" ];then
 	# Set to .* to allow sending files from all locations
 	FILE_REGEX='/home/user/allowed/.*'
 else
-	send_action "${CHAT[ID]}" "typing"
 	if ! tmux ls | grep -v send | grep -q $copname; then
-		[ ! -z ${URLS[*]} ] && {
-			send_message "${CHAT[ID]}" "Initiating recognition process..."
-			curl -s ${URLS[*]} -o /tmp/$ME$NAME
-			send_message "${CHAT[ID]}" "$(../audiokeychain.sh "/tmp/$ME$NAME")"
+		TOCHECK="${URLS[AUDIO]} ${URLS[DOCUMENT]}"
+		[ ! -z $TOCHECK ] && {
+			send_action "${CHAT[ID]}" "typing"
+#			send_message "${CHAT[ID]}" "Initiating recognition process..."
+			curl -s $TOCHECK -o /tmp/$ME$NAME
+			CHECKRES="$(../audiokeychain.sh "/tmp/$ME$NAME")"
+			#[ ! -z $CHECKRES ] && 
+			res=$(curl -s "$MSG_URL" -d "chat_id=${CHAT[ID]}" -d "text=$(urlencode "$CHECKRES")" -d "reply_to_message_id=$MESSAGE_ID")
 			rm "/tmp/$ME$NAME"
 			return
 		}
@@ -27,7 +30,8 @@ else
 		'/info')
 			send_markdown_message "${CHAT[ID]}" "This is bashbot, the *Telegram* bot written entirely in *bash*."
 			;;
-		*)
+		'/start')
+			send_action "${CHAT[ID]}" "typing"
 			send_message "${CHAT[ID]}" "This bot can recognize the musical key and the bpm (beats per minute) of any song.
 To start, send me an audio file in mp3 or wav format smaller than 15 megabytes.
 
@@ -35,7 +39,7 @@ Available commands:
 • /start: Start bot and get this message.
 
 This bot uses audiokeychain.com to recognize the songs. Both the audiokeychain client and the bot is written by Daniil Gentili (@danogentili).
-Check out my other bots: @video_dl_bot, @mklwp_bot, @caption_ai_bot, @cowsaysbot, @cowthinksbot, @figletsbot, @lolcatzbot, @filtersbot, @id3bot, @pwrtelegrambot
+Check out my other bots: @video_dl_bot, @mklwp_bot, @caption_ai_bot, @cowsaysbot, @cowthinksbot, @figletsbot, @lolcatzbot, @filtersbot, @id3bot, @pwrtelegrambot, @lennysbot
 Source code: https://github.com/danog/audiokeychain-clients
 "
 			;;
